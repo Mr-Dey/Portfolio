c8ff115d4843f46728e764ffe2950f8113da7aa4
Mr-Dey <subhasishdeycoc@gmail.com>
2024-05-14 21:41:40 +0530

    updated UI

--- script/functions.js
+++ script/functions.js
@@ -1,8 +1,12 @@
+let home=document.querySelector("#navbar_home");
 let project=document.querySelector("#navbar_project");
+let skills=document.querySelector("#navbar_skills");
+let firstSection=document.querySelector("#firstSection");
 let secondSection=document.querySelector('#secondSection');
+let thirdSection=document.querySelector("#thirdSection");
 let section=document.querySelectorAll(".section")
 
-// functions
+// functions for scroll
 scroll=(element,offset)=>{
     window.scrollTo({
         top:element.offsetTop-offset,
@@ -10,15 +14,16 @@ scroll=(element,offset)=>{
     })
 }
 
+//The main Function to show smooth blur transition
 const observer=new IntersectionObserver((elemet)=>{
     elemet.forEach((e)=>{
         e.isIntersecting?e.target.classList.add('show'):e.target.classList.remove('show');
     })
 })
 
+home.addEventListener('click',()=>scroll(firstSection,100));
+project.addEventListener('click',()=>scroll(secondSection,100));
+skills.addEventListener('click',()=>scroll(thirdSection,100));
 
-project.addEventListener('click',()=>{
-    scroll(secondSection,100);
-})
 
 section.forEach((e)=>observer.observe(e));
