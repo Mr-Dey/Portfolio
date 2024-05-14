5040d27e299fb1bec63384f0191e283ddd056aca
Mr-Dey <subhasishdeycoc@gmail.com>
2024-05-14 09:20:17 +0530

    added animations

--- script/functions.js
+++ script/functions.js
@@ -1,6 +1,8 @@
 let project=document.querySelector("#navbar_project");
 let secondSection=document.querySelector('#secondSection');
+let section=document.querySelectorAll(".section")
 
+// functions
 scroll=(element,offset)=>{
     window.scrollTo({
         top:element.offsetTop-offset,
@@ -8,6 +10,15 @@ scroll=(element,offset)=>{
     })
 }
 
+const observer=new IntersectionObserver((elemet)=>{
+    elemet.forEach((e)=>{
+        e.isIntersecting?e.target.classList.add('show'):e.target.classList.remove('show');
+    })
+})
+
+
 project.addEventListener('click',()=>{
     scroll(secondSection,100);
 })
+
+section.forEach((e)=>observer.observe(e));
